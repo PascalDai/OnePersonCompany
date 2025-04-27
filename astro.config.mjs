71fb862983512476fc96fd347723e8998a349a5b
Pascal Dai <daiyi@westlake.edu.cn>
2025-04-27 16:21:53 +0800

    更新配置文件和博客内容

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+
+import remarkMermaid from "remark-mermaidjs";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   legacy: {
     collections: true,
+  },
+  markdown: {
+    remarkPlugins: [remarkMermaid],
   },
 });
